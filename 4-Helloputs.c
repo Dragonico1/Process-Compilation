4a1e6df04a692507eabdc64241e5db59b98d0776
root <root@DESKTOP-IC61I73.localdomain>
2023-03-27 15:46:40 -0500

    Tareas 2

---
+++ 4-Helloputs.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main() {
+
+    puts("\"Programming is like building a multilingual puzzle");
+
+}
---
+++ 5-printf.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+/**
+
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void) 
+{
+
+    prinft(" but the outcome is a pice of art,\n");
+    return(0);
+
+}
